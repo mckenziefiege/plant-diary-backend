39fa685406c82a877dc427ece362822ad1fe4833
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-08-14 09:20:02 -0400

    modified the plant and comment serializers to include comments for each plant

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,3 +1,5 @@
 class PlantSerializer < ActiveModel::Serializer
-  attributes :id, :nickname, :species, :image, :location, :user_id, :watering_frequency, :date_obtained
+  attributes :id, :nickname, :species, :image, :location, :user_id, :watering_frequency, :date_obtained, :comments
+  has_many :comments
+  belongs_to :user
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :username, :image
+  has_many :plants
+  has_many :tasks
 end
