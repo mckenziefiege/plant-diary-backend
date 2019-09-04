c78a2482a33aca9b80d7b2cc67b458a05d000b4d
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-09-04 17:45:34 -0400

    added readable_time attribute to Comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -2,4 +2,7 @@ class CommentSerializer < ActiveModel::Serializer
   attributes :id, :content, :plant_id, :user_id
   belongs_to :plant
   belongs_to :user
+  attribute :readable_time do
+    object.created_at.strftime("%m/%d/%Y at %I:%M%p")
+  end
 end
